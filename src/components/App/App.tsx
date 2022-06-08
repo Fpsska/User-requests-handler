5ce4dda06d2db5e4f477c08850d5f923f3a912ac
Fpsska <fpsska1337@gmail.com>
2022-06-08 14:43:03 +0300

    feat: create _style.scss file with null-styles & install react-router-dom, react-router npm package

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,16 @@
-import './App.css';
+import React from 'react';
 
-function App() {
+import './App.css';
+import '../../assets/styles/_style.scss';
+
+// /. imports
+
+const App: React.FC = () => {
   return (
     <div className="App">
 
     </div>
   );
-}
+};
 
 export default App;
