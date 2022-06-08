678fa8027635eac4041449b90c383fd2751ae032
Fpsska <fpsska1337@gmail.com>
2022-06-08 16:04:46 +0300

    refactor: HTML-structure of Form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { FaSort } from 'react-icons/fa';
+
 import './form.scss';
 
 // /. imports
@@ -8,20 +10,45 @@ const Form: React.FC = () => {
     return (
         <form className="form" action="#">
             <div className="form__wrapper">
+
                 <div className="form__template">
-                    <input className="form__input form__input--id" type="text" placeholder="ID" id="ID" />
+                    <label className="form__label" htmlFor="ID">
+                        <input className="form__input form__input--id" type="text" placeholder="ID" id="ID" />
+                        <div className="form__controls">
+                            <span className="form__text">ID</span>
+                            <FaSort />
+                        </div>
+                    </label>
                 </div>
 
                 <div className="form__template">
-                    <input className="form__input form__input--fio" type="text" placeholder="ФИО" id="FIO" />
+                    <label className="form__label" htmlFor="FIO">
+                        <input className="form__input form__input--fio" type="text" placeholder="ФИО" id="FIO" />
+                        <div className="form__controls">
+                            <span className="form__text">ФИО</span>
+                            <FaSort />
+                        </div>
+                    </label>
                 </div>
 
                 <div className="form__template">
-                    <input className="form__input form__input--birth" type="text" placeholder="Дата рождения" id="BIRTH" />
+                    <label className="form__label" htmlFor="BIRTH">
+                        <input className="form__input form__input--birth" type="text" placeholder="Дата рождения" id="BIRTH" />
+                        <div className="form__controls">
+                            <span className="form__text">Дата рождения</span>
+                            <FaSort />
+                        </div>
+                    </label>
                 </div>
 
                 <div className="form__template">
-                    <input className="form__input form__input--phone" type="number" placeholder="Телефон" id="PHONE" />
+                    <label className="form__label" htmlFor="PHONE">
+                        <input className="form__input form__input--phone" type="number" placeholder="Телефон" id="PHONE" />
+                        <div className="form__controls">
+                            <span className="form__text">Телефон</span>
+                            <FaSort />
+                        </div>
+                    </label>
                 </div>
 
                 <div className="form__template">
@@ -30,6 +57,10 @@ const Form: React.FC = () => {
                         <option className="">Филиал №1</option>
                         <option className="">Филиал №2</option>
                     </select>
+                    <div className="form__controls">
+                        <span className="form__text">Филиал</span>
+                        <FaSort />
+                    </div>
                 </div>
 
                 <div className="form__template">
@@ -38,6 +69,17 @@ const Form: React.FC = () => {
                         <option className="selection-menu__option">оплачено</option>
                         <option className="selection-menu__option">не оплачено</option>
                     </select>
+                    <div className="form__controls">
+                        <span className="form__text">Оплата</span>
+                        <FaSort />
+                    </div>
+                </div>
+
+                <div className="form__template">
+                    <div className="form__controls">
+                        <span className="form__text">Статус</span>
+                        <FaSort />
+                    </div>
                 </div>
 
             </div>
